b326417bf198cc6fae2d056f412d56267d19f862
Eduardo Alvim de Carvalho <eduardoac@gmail.com>
2016-10-07 18:24:07 -0300

    Refactor

--- app/view2/view2_test.js
+++ app/view2/view2_test.js
@@ -1,8 +1,8 @@
 'use strict';
 
-describe('myApp.view2 module', function() {
+describe('portalMovie.view2 module', function() {
 
-  beforeEach(module('myApp.view2'));
+  beforeEach(module('portalMovie.view2'));
 
   describe('view2 controller', function(){
 
